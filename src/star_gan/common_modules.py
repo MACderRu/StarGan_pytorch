75f549abc89339bdf1b4e7970f0356e0b6044ec3
Vadim Titov <titov.vn@phystech.edu>
2021-04-03 18:52:16 +0300

    FIX: relu act () removed in residual

--- src/star_gan/common_modules.py
+++ src/star_gan/common_modules.py
@@ -109,7 +109,7 @@ class BaseResidualBlock(nn.Module):
             kernel_size=kernel_size,
             stride=stride,
             norm=True,
-            act=nn.ReLU()
+            act=nn.ReLU
         )
 
         self.block2 = BaseConvBlock(
